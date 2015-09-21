8f3b2f778129384430683bd9cb649f832bbdfcee
Руслан Усманов <extiser@MacBook-Pro-Ruslan.local>
2015-09-21 11:32:33 +0500

    fixes

--- markup/modules/top-menu/top-menu.js
+++ markup/modules/top-menu/top-menu.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
     $('.top-menu__item').click(function () {
         var blockId = $(this).attr('href');
+        var headerHeight = $('.header').height();
         var destination = $(blockId).offset().top;
-        $('body').animate({ scrollTop: destination - 80 }, 1100);
+        $('body').animate({ scrollTop: destination - headerHeight - 40 }, 1100);
         $('.top-menu__list').removeClass('active');
         return false;
     });
